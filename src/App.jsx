f70b0f543cbeb0a2b6aac8dc4895c97d226b7ebf
kavyachetwani <kavya422chetwani@gmail.com>
2025-01-18 10:56:09 +0530

    Ehh

--- src/App.jsx
+++ src/App.jsx
@@ -62,9 +62,9 @@ const AppContent = () => {
 
 export default function App() {
   return (
-    // <Router>
-    //   <AppContent />
-    // </Router>
-    <CustomizationForm/>
+    <Router>
+      <AppContent />
+    </Router>
+    // <CustomizationForm/>
   );
 }

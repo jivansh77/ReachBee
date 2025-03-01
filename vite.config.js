277108c2be0b9da5afef3265a13220cbd651fd61
jivansh77 <jivansh777@gmail.com>
2025-03-01 11:10:48 +0530

    ..

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    exclude: ['fs', 'https', 'zlib', 'crypto', 'twitter-api-v2']
+  },
+  build: {
+    rollupOptions: {
+      external: ['fs', 'https', 'zlib', 'crypto']
+    }
+  },
+  ssr: {
+    noExternal: ['twitter-api-v2']
+  }
 })
